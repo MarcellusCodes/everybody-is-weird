f9e5beaa15d0c42e9abb9d677e36b447b20d4cbf
MarcellusCodes <MarcellusCodes@gmail.com>
2022-08-09 19:43:12 +0200

    - create excalidraw mockup X

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E2961",
+        secondary: {
+          primary: "#56ab2f",
+          secondary: "#a8e063",
+        },
+        tertiary: {
+          primary: "#56CCF2",
+          secondary: "#2F80ED",
+        },
+        quaternary: {
+          primary: "#FBD3E9",
+          secondary: "#BB377D",
+        },
+      },
+    },
+    fontFamily: {
+      primary: ["Mouse Memoirs", "sans-serif"],
+      secondary: ["Roboto Slab", "serif"],
+    },
+  },
+  plugins: [],
+};
